a681dc750511f5961bef26098ec8fdd69fff470a
Rishabh Joshi <rjoshi4@buffalo.edu>
2019-10-13 18:39:07 -0400

    removing print statements

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -214,7 +214,7 @@ thread_create (const char *name, int priority,
     thread_unblock (t);
     struct thread *current_thread = thread_current ();
   if(current_thread->priority < t->priority){
-      printf(" Hi I am here in this thred with priority %d and current thread priority %d      ",t->priority, current_thread->priority);
+//      printf(" Hi I am here in this thred with priority %d and current thread priority %d      ",t->priority, current_thread->priority);
       thread_yield_priority (current_thread);
   }
     return tid;
@@ -255,7 +255,7 @@ thread_unblock (struct thread *t) {
 
     // Doing changes for priority
 //    if (t != ideal_thread) {
-    printf(" thread being pushed is %d   ", t->priority);
+//    printf(" thread being pushed is %d   ", t->priority);
     list_insert_ordered(&ready_list, &t->elem, ordered_priority_dsc, NULL);
 //    struct list_elem * e;
 //    for (e = list_begin (&ready_list); e != list_end (&ready_list);
