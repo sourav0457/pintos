ddc514bfa807e3f54a16f5770e47a6de4049c7b3
Rishabh Joshi <rjoshi4@buffalo.edu>
2019-10-13 12:33:43 -0400

    adding current thread in ordered list rather than push_back

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -377,8 +377,9 @@ thread_yield (void)
   ASSERT (!intr_context ());
 
   old_level = intr_disable ();
-  if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+  if (cur != idle_thread)
+      list_insert_ordered(&ready_list, &cur->elem, ordered_priority_dsc, NULL);
+//    list_push_back (&ready_list, &cur->elem);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
