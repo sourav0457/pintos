61d63bd032d7ca2afd62b422e4a1706d2c3e6fa3
Sourav Samanta <sourav0457@gmail.com>
2019-10-20 00:36:09 -0400

    Changing fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -8,18 +8,19 @@
 #define F (1 << 14)
 
 // Conversions
-int int_to_fp (int n) {return n * F;}
-int fp_to_int_towards_zero (int n) {return n / F;}
-int fp_to_int_towards_nearest  (int n) {return (n >= 0 ? (n + F / 2) / F : (n - F / 2) / F);}
+#define int_to_fp (n) ((n) * (F))
+#define fp_to_int_towards_zero (n) ((n) / (F))
+#define fp_to_int_towards_nearest  (n) ((n) >= 0) ? (((n) + (F / 2)) / (F)) : (((n) - (F / 2)) / (F))
 
 // Arithematic Operations
-int add_fp_fp (int x, int y) {return x + y;}
-int sub_fp_fp (int x, int y) {return x - y;}
-int mul_fp_fp (int x, int y) {return ((int64_t) x) * y / F;}
-int div_fp_fp (int x, int y) {return ((int64_t) x) * F / y;}
-int add_fp_int (int x, int n) {return x + n * F;}
-int sub_fp_int (int x, int n) {return x - n * F;}
-int mul_fp_int (int x, int n) {return x * n;}
-int div_fp_int (int x, int n) {return x / n;}
+#define add_fp_fp (x, y) ((x) + (y))
+#define sub_fp_fp (x, y) ((x) - (y))
+
+#define mul_fp_fp (x, y) ((((int64_t) x) * (y))/ (F))
+#define div_fp_fp (x, y) ((((int64_t) x) * (F)) / (y))
+#define add_fp_int (x, n) ((x) + ((n) * F))
+#define sub_fp_int (x, n) ((x) - ((n) * F))
+#define mul_fp_int (x, n) ((x) * (n))
+#define div_fp_int (x, n) ((x) / (n))
 
 #endif
