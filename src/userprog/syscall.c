2dd2e99219906f616fd1b093120ac66941ec8ad3
Sourav Samanta <sourav0457@gmail.com>
2019-11-30 23:28:25 -0500

    Implementing wait and create system calls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,8 +4,14 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "devices/shutdown.h"
+#include "process.h"
+#include "userprog/pagedir.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
+static void exit(int exit_status);
 struct file_descriptor_mapper{
     struct list_elem elem_file;
     struct file * address;
@@ -30,13 +36,63 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+  int *addr = f->esp;
+  int args[3];
+  void * phys_page_ptr;
+  is_valid_add((const void*) addr);
+  switch(*addr){
+    case SYS_HALT:
+      shutdown_power_off();
+    case SYS_WAIT:
+      is_valid_add((const void *) ++addr);
+      f->eax = process_wait(*addr);
+      break;
+    case SYS_CREATE:
+      // int *temp = addr;
+      // for(int i=0; i< 2; i++, temp++){
+      //   is_valid_add((const void*) temp);
+      // }
+      // is_valid_add((const void *) *addr);
+      get_stack_arguments(f, &args[0], 2);
+      check_buffer((void *)args[0], args[1]);
+      phys_page_ptr=pagedir_get_page(thread_current()->pagedir, (const void *) args[0]);
+      if(phys_page_ptr == NULL){
+        exit(-1);
+      }
+      args[0] = (int) phys_page_ptr;
+      lock_acquire(&file_lock);
+      f->eax = filesys_create((const char *) args[0], (unsigned) args[1]);
+      lock_release(&file_lock);
+      break;
+  }
   thread_exit ();
 }
 
 void exit (int exit_status)
 {
     thread_current()->exit_status = exit_status;
-//    printf("%s: exit(%d)\n", thread_current()->name, status);
     thread_exit ();
+}
+
+void check_buffer (void *buff_to_check, unsigned size)
+{
+  unsigned i;
+  char *ptr  = (char * )buff_to_check;
+  for (i = 0; i < size; i++)
+    {
+      check_valid_addr((const void *) ptr);
+      ptr++;
+    }
+}
+
+void get_stack_arguments (struct intr_frame *f, int *args, int num_of_args)
+{
+  int i;
+  int *ptr;
+  for (i = 0; i < num_of_args; i++)
+    {
+      ptr = (int *) f->esp + i + 1;
+      check_valid_addr((const void *) ptr);
+      args[i] = *ptr;
+    }
 }
