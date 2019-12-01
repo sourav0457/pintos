39714fbfa0f47f03fec9926fbac5e0db96a2eeea
Rishabh Joshi <rjoshi4@buffalo.edu>
2019-11-30 21:35:10 -0500

    change in thread_exit

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,12 +3,27 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+struct file_descriptor_mapper{
+    struct list_elem elem_file;
+    struct file * address;
+    int file_descriptor;
+};
+
+struct lock file_lock;
+
+void is_valid_add(const void * ptr){
+    if(ptr == NULL || ptr<(void * )0x08048000 || !is_user_vaddr(ptr)){
+        exit(-1);
+    }
+}
 
 void
 syscall_init (void) 
 {
+  lock_init(&file_lock);
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -17,4 +32,11 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+void exit (int exit_status)
+{
+    thread_current()->exit_status = exit_status;
+//    printf("%s: exit(%d)\n", thread_current()->name, status);
+    thread_exit ();
 }
